b84b6d0fa6e6bc1022edd7d659a12edaca1d7b4b
Omer Demir <odemir@valven.com>
2024-07-17 13:35:24 +0300

    TST-22 linear commit new

--- test27.py
+++ test27.py
@@ -2118,3 +2118,9 @@ class BaseEventLoop(events.AbstractEventLoop):
 
         if self.is_running():
             self.call_soon_threadsafe(self._set_coroutine_origin_tracking, enabled)
+
+    def test_function_12(self, enabled):
+        self._debug = enabled
+
+        if self.is_running():
+            self.call_soon_threadsafe(self._set_coroutine_origin_tracking, enabled)
