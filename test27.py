76836937bd33fe339cb80cf8c3b7543bf39f55e2
Omer Demir <odemir@valven.com>
2024-07-17 14:16:34 +0300

    #33 issue 4 commit github

--- test27.py
+++ test27.py
@@ -2160,3 +2160,9 @@ class BaseEventLoop(events.AbstractEventLoop):
 
         if self.is_running():
             self.call_soon_threadsafe(self._set_coroutine_origin_tracking, enabled)
+
+    def test_function_19(self, enabled):
+        self._debug = enabled
+
+        if self.is_running():
+            self.call_soon_threadsafe(self._set_coroutine_origin_tracking, enabled)
